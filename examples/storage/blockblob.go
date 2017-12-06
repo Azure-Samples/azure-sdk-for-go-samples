8534c6be5588f404b078cfa13ccd2892b2c2dc1c
mcardosos <mcardoso@microsoft.com>
2017-12-06 12:36:04 -0800

    Fixed storage tests

--- examples/storage/blockblob.go
+++ examples/storage/blockblob.go
@@ -4,34 +4,14 @@ import (
 	"context"
 	"fmt"
 	"net/url"
-	"os"
 	"strings"
 
 	blob "github.com/Azure/azure-storage-blob-go/2016-05-31/azblob"
-	"github.com/subosito/gotenv"
 )
 
-var (
-	blobPath string
-	blobName string
-)
-
-func init() {
-	gotenv.Load()
-	blobName = os.Getenv("AZURE_STORAGE_BLOBNAME")
-	if len(blobName) < 1 {
-		blobName = "myblob"
-	}
-
-	blobPath = os.Getenv("AZURE_STORAGE_BLOBPATH")
-	if len(blobPath) < 1 {
-		blobPath = "./_testdata/blob.bin"
-	}
-}
-
-func getBlockBlobURL(containerName string, blobName string) blob.BlockBlobURL {
-	loadKey()
-	c := blob.NewSharedKeyCredential(accountName, accountKey)
+func getBlockBlobURL(accountName, containerName, blobName string) blob.BlockBlobURL {
+	key := loadKey(accountName)
+	c := blob.NewSharedKeyCredential(accountName, key)
 	p := blob.NewPipeline(c, blob.PipelineOptions{})
 	u, _ := url.Parse(fmt.Sprintf(blobFormatString, accountName))
 	service := blob.NewServiceURL(*u, p)
@@ -41,11 +21,8 @@ func getBlockBlobURL(containerName string, blobName string) blob.BlockBlobURL {
 }
 
 // CreateBlockBlob creates a new test blob in the container specified by env var
-func CreateBlockBlob(name string) (blob.BlockBlobURL, error) {
-	if len(name) > 0 {
-		blobName = name
-	}
-	b := getBlockBlobURL(containerName, blobName)
+func CreateBlockBlob(accountName, containerName, blobName string) (blob.BlockBlobURL, error) {
+	b := getBlockBlobURL(accountName, containerName, blobName)
 	data := "blob created by Azure-Samples, okay to delete!"
 
 	_, err := b.PutBlob(
--- examples/storage/storage_test.go
+++ examples/storage/storage_test.go
@@ -1,47 +1,59 @@
 package storage
 
 import (
-	"fmt"
+	"flag"
 	"log"
+	"strings"
+	"testing"
 
-	_ "github.com/Azure-Samples/azure-sdk-for-go-samples/helpers"
-	"github.com/Azure-Samples/azure-sdk-for-go-samples/resources"
+	"github.com/Azure-Samples/azure-sdk-for-go-samples/examples/resources"
+	"github.com/Azure-Samples/azure-sdk-for-go-samples/management"
+	"github.com/marstr/randname"
+	chk "gopkg.in/check.v1"
 )
+
+var (
+	accountName   string
+	containerName string
+	blobName      string
+)
+
+func init() {
+	management.GetStartParams()
+	flag.StringVar(&accountName, "storageAccName", "acc"+randname.AdjNoun{}.Generate(), "Provide a name for the storage account to be created")
+	flag.StringVar(&containerName, "containerName", "cnt"+randname.AdjNoun{}.Generate(), "Provide a name for the storage account to be created")
+	flag.StringVar(&blobName, "blobName", "blob"+randname.AdjNoun{}.Generate(), "Provide a name for the storage account to be created")
+	flag.Parse()
+}
+
+func Test(t *testing.T) { chk.TestingT(t) }
+
+type StorageSuite struct{}
+
+var _ = chk.Suite(&StorageSuite{})
 
 // Example creates a resource group and a storage account. Then it adds a container and a blob in that account.
 // Finally it removes the blob, container, account, and group.
 // more examples available at https://github.com/Azure/azure-storage-blob-go/2016-05-31/azblob/zt_examples_test.go
-func Example() {
-	var err error
-	var errC <-chan error
-
+func (s *StorageSuite) TestUploadBlockBlob(c *chk.C) {
 	defer resources.Cleanup()
 
 	group, err := resources.CreateGroup()
-	if err != nil {
-		log.Fatalf("failed to get create group: %v", err)
-	}
-	log.Printf("created group: %v\n", group)
+	c.Check(err, chk.IsNil)
+	log.Printf("group: %+v\n", group)
 
-	account, errC := CreateStorageAccount()
-	err = <-errC // wait on error channel
-	if err != nil {
-		log.Fatalf("failed to create storage account: %v", err)
-	}
+	accountName = strings.ToLower(accountName)
+	containerName = strings.ToLower(containerName)
+
+	account, errC := CreateStorageAccount(accountName)
+	c.Check(<-errC, chk.IsNil)
 	log.Printf("created storage account: %v\n", <-account)
 
-	c, err := CreateContainer(containerName)
-	if err != nil {
-		log.Fatalf("failed to create container: %v", err)
-	}
-	log.Printf("created container: %v", c)
+	cnt, err := CreateContainer(accountName, containerName)
+	c.Check(err, chk.IsNil)
+	log.Printf("created container: %v", cnt)
 
-	b, err := CreateBlockBlob(blobName)
-	if err != nil {
-		log.Fatalf("failed to create blob: %v", err)
-	}
+	b, err := CreateBlockBlob(accountName, containerName, blobName)
+	c.Check(err, chk.IsNil)
 	log.Printf("created blob: %v", b)
-
-	fmt.Println("Success")
-	// Output: Success
 }

5491924f5bb6d9f4fc6f897f4eba8fa0930fbb91
Mariana Cardoso <mcardoso@microsoft.com>
2018-01-03 16:48:30 -0800

    Added travis file (#51)
    * Added travis file
    
    * Fixed gofmt
    
    * Disable gofmt
    
    * Use golist instead
    
    * Fixes golint
    
    * Is gobuild working correctly?
    
    * Simplify?
    
    * Only use latest go version
    
    * Many fixes
    
    * Change order
    
    * Some more fixes
    
    * Tweak

--- resources/cleanup.go
+++ resources/cleanup.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Azure-Samples/azure-sdk-for-go-samples/helpers"
 )
 
+// Cleanup deletes the rescource group created for the sample
 func Cleanup(ctx context.Context) error {
 	if helpers.KeepResources() {
 		log.Println("keeping resources")

c53b1bf1ce1dc4b3650939dc1001751b0a104006
Mariana Cardoso <mcardoso@microsoft.com>
2018-01-08 16:08:03 -0800

    Location (#56)
    * Override location
    
    * Adding a package that cleans the subscription (this is useful for CI)
    
    * Fix quiet flag
    
    * Feedback
    
    * Fixed Travis
    
    * Fix CI
    
    * Add print

---
+++ tools/ci.sh
@@ -0,0 +1,19 @@
+# this script runs the samples, and cleans thse subscription
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    echo 'skip running tests'
+    exit 0
+fi
+
+REALEXITSTATUS=0
+
+dirs=$(go list ./...)
+test -z "`for d in $dirs; do go test -v $d | tee /dev/stderr; done`"
+REALEXITSTATUS=$(($REALEXITSTATUS+$?))
+
+go install github.com/Azure-Samples/azure-sdk-for-go-samples/tools/cleanup
+REALEXITSTATUS=$(($REALEXITSTATUS+$?))
+
+cleanup -quiet
+REALEXITSTATUS=$(($REALEXITSTATUS+$?))
+
+exit $REALEXITSTATUS

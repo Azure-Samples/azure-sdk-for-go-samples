2d58ad598f976b8fdd9e485f17e912fce0e2d936
Josh Gavant <joshgavant@gmail.com>
2018-01-11 15:48:53 -0800

    add license headers

--- sql/queries.go
+++ sql/queries.go
@@ -1,3 +1,8 @@
+// Copyright (c) Microsoft and contributors.  All rights reserved.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
 // Package sql tests basic functionality for an existing mssql db
 package sql
 

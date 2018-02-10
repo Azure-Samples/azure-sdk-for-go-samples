69357183a5ed096f5ccc66c02552fc33fe2b9608
Stephen Tramer <sttramer@microsoft.com>
2018-02-09 16:10:11 -0800

    Change to use service versions rather than profiles.

--- quickstarts/deploy-vm/main.go
+++ quickstarts/deploy-vm/main.go
@@ -6,8 +6,8 @@ import (
 	"io/ioutil"
 	"log"
 
-	"github.com/Azure/azure-sdk-for-go/profiles/2017-03-09/network/mgmt/network"
-	"github.com/Azure/azure-sdk-for-go/profiles/2017-03-09/resources/mgmt/resources"
+	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2017-09-01/network"
+	"github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2017-05-10/resources"
 
 	"github.com/Azure/go-autorest/autorest"
 	"github.com/Azure/go-autorest/autorest/adal"

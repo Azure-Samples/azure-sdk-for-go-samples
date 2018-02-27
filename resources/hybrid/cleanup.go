6da4f92e89f30aaf8d1b46dda53a28a3be9c8b43
seyadava <seyadava@microsoft.com>
2018-02-27 15:18:31 -0800

    added resource samples for hybrid scenario

---
+++ resources/hybrid/cleanup.go
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft and contributors.  All rights reserved.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package hybridresources
+
+import (
+	"context"
+	"log"
+
+	"github.com/Azure-Samples/azure-sdk-for-go-samples/helpers"
+)
+
+// Cleanup deletes the rescource group created for the sample
+func Cleanup(ctx context.Context) error {
+	if helpers.KeepResources() {
+		log.Println("keeping resources")
+		return nil
+	}
+	log.Println("deleting resources")
+	_, err := DeleteGroup(ctx, helpers.ResourceGroupName())
+	return err
+}

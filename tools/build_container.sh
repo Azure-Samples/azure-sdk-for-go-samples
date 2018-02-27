6776e7708e4211f1c7fb3aedb88ad4aa0e587e60
Mariana Cardoso <mcardoso@microsoft.com>
2018-02-26 16:56:54 -0800

    Feedback

--- tools/build_container.sh
+++ tools/build_container.sh
@@ -1,7 +1,3 @@
-curl -sL https://a01tools.blob.core.windows.net/droid/linux/a01droid -o tools/a01droid
-chmod +x tools/a01droid
-chmod +x tools/get_index
-
 image_owner=${TRAVIS_REPO_SLUG%-Samples/azure-sdk-for-go-samples} 
 image_owner=${image_owner:="private-${USER}"}
 image_owner=`echo $image_owner | tr '[:upper:]' '[:lower:]'`
@@ -9,9 +5,9 @@ image_owner=`echo $image_owner | tr '[:upper:]' '[:lower:]'`
 build=$TRAVIS_BUILD_NUMBER
 build=${build:=`date +%Y%m%d%H%M`}
 
-mv tools/dockerfiles/* .
+mv tools/Dockerfile .
 
-docker build -t azureclidev.azurecr.io/azuresdk-test-$image_owner:go1.10-$build -f Dockerfile.1.10 .
+docker build -t azureclidev.azurecr.io/azuresdk-test-$image_owner:go1.10-$build .
 docker push azureclidev.azurecr.io/azuresdk-test-$image_owner:go1.10-$build
 
-mv `ls | grep ^Dockerfile` tools/dockerfiles
+mv Dockerfile tools

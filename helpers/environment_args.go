93a4ff010930ce86b0d0443d44ac9a64636b958d
seyadava <seyadava@microsoft.com>
2018-02-27 15:18:31 -0800

    added resource samples for hybrid scenario

---
+++ helpers/environment_args.go
@@ -0,0 +1,53 @@
+// Copyright (c) Microsoft and contributors.  All rights reserved.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package helpers
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+// Audience represents list of audience endpoints
+type Audience []string
+
+// Authentication represents authentication section of Environment Information
+type Authentication struct {
+	LoginEndpoint string   `json:"loginEndpoint"`
+	Audiences     Audience `json:"audiences"`
+}
+
+// EnvironmentInformation represents a set of endpoints for the of Azure's Environment.
+type EnvironmentInformation struct {
+	GalleryEndpoint string         `json:"galleryEndpoint"`
+	GraphEndpoint   string         `json:"graphEndpoint"`
+	PortalEndpoint  string         `json:"portalEndpoint"`
+	Authentication  Authentication `json:"authentication"`
+}
+
+var client = &http.Client{Timeout: 3 * time.Second}
+
+// GetAadResourceID retrieves AadResourceId from ARMEndpoint
+func GetAadResourceID(armEndpointString string, aadResourceID interface{}, aadEndpoint interface{}) error {
+	managementEndpoint := armEndpointString + "/metadata/endpoints?api-version=1.0"
+	env := new(EnvironmentInformation)
+	if err := GetJSON(managementEndpoint, env); err != nil {
+		return err
+	}
+	aadResourceID = env.Authentication.Audiences[0]
+	aadEndpoint = env.Authentication.LoginEndpoint
+	return nil
+}
+
+// GetJSON retrieves EnvironmentInformation
+func GetJSON(url string, target interface{}) error {
+	response, err := client.Get(url)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+	return json.NewDecoder(response.Body).Decode(target)
+}
---
+++ resources/hybrid/resources_test.go
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft and contributors.  All rights reserved.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package hybridresources
+
+import (
+	"context"
+
+	"github.com/Azure-Samples/azure-sdk-for-go-samples/helpers"
+)
+
+// func init() {
+// 	err := helpers.ParseArgs()
+// 	if err != nil {
+// 		log.Fatalf("cannot parse arguments: %v", err)
+// 	}
+// }
+
+func ExampleCreateGroup() {
+	defer Cleanup(context.Background())
+
+	_, err := CreateGroup(context.Background(), helpers.ResourceGroupName())
+	if err != nil {
+		helpers.PrintAndLog(err.Error())
+	}
+	helpers.PrintAndLog("resource group created")
+
+	// Output:
+	// resource group created
+}

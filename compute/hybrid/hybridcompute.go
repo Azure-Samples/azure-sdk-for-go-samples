fc6e8880189ff6e50c824bd005b844f0aea5ac29
AzureStackAdmin@AZURESTACK.LOCAL <AzureStackAdmin@AZURESTACK.LOCAL>
2018-02-28 13:33:19 -0800

    add compute hybrid samples

---
+++ compute/hybrid/hybridcompute.go
@@ -0,0 +1,126 @@
+// Copyright (c) Microsoft and contributors.  All rights reserved.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package hybridcompute
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/Azure-Samples/azure-sdk-for-go-samples/helpers"
+	"github.com/Azure-Samples/azure-sdk-for-go-samples/iam"
+	hybridnetwork "github.com/Azure-Samples/azure-sdk-for-go-samples/network/hybrid"
+	"github.com/Azure/azure-sdk-for-go/profiles/2017-03-09/compute/mgmt/compute"
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest/to"
+)
+
+const (
+	publisher = "Canonical"
+	offer     = "UbuntuServer"
+	sku       = "16.04-LTS"
+)
+
+var (
+	location = "local"
+)
+
+// fakepubkey is used if a key isn't available at the specified path in CreateVM(...)
+var fakepubkey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC7laRyN4B3YZmVrDEZLZoIuUA72pQ0DpGuZBZWykCofIfCPrFZAJgFvonKGgKJl6FGKIunkZL9Us/mV4ZPkZhBlE7uX83AAf5i9Q8FmKpotzmaxN10/1mcnEE7pFvLoSkwqrQSkrrgSm8zaJ3g91giXSbtqvSIj/vk2f05stYmLfhAwNo3Oh27ugCakCoVeuCrZkvHMaJgcYrIGCuFo6q0Pfk9rsZyriIqEa9AtiUOtViInVYdby7y71wcbl0AbbCZsTSqnSoVxm2tRkOsXV6+8X4SnwcmZbao3H+zfO1GBhQOLxJ4NQbzAa8IJh810rYARNLptgmsd4cYXVOSosTX azureuser"
+
+func getVMClient() (compute.VirtualMachinesClient, error) {
+	token, err := iam.GetResourceManagementTokenHybrid(helpers.ActiveDirectoryEndpoint(), helpers.TenantID(), helpers.ClientID(), helpers.ClientSecret(), helpers.ActiveDirectoryResourceID())
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Cannot generate token. Error details: %s.", err.Error()))
+	}
+	vmClient := compute.NewVirtualMachinesClientWithBaseURI(helpers.ArmEndpoint(), helpers.SubscriptionID())
+	vmClient.Authorizer = autorest.NewBearerAuthorizer(token)
+	return vmClient, nil
+}
+
+// CreateVM creates a new virtual machine with the specified name using the specified NIC.
+// Username, password, and sshPublicKeyPath determine logon credentials.
+func CreateVM(ctx context.Context, vmName, nicName, username, password, storageAccountName, sshPublicKeyPath string) (vm compute.VirtualMachine, err error) {
+	cntx := context.Background()
+	nic, _ := hybridnetwork.GetNic(cntx, nicName)
+
+	var sshKeyData string
+	_, err = os.Stat(sshPublicKeyPath)
+	if err == nil {
+		sshBytes, err := ioutil.ReadFile(sshPublicKeyPath)
+		if err != nil {
+			log.Fatalf("failed to read SSH key data: %v", err)
+		}
+		sshKeyData = string(sshBytes)
+	} else {
+		sshKeyData = fakepubkey
+	}
+	vhdURItemplate := "https://%s.blob." + helpers.StorageEndpointSuffix() + "/vhds/%s.vhd"
+	vmClient, _ := getVMClient()
+	future, err := vmClient.CreateOrUpdate(
+		cntx,
+		helpers.ResourceGroupName(),
+		vmName,
+		compute.VirtualMachine{
+			Location: to.StringPtr(location),
+			VirtualMachineProperties: &compute.VirtualMachineProperties{
+				HardwareProfile: &compute.HardwareProfile{
+					VMSize: compute.StandardA1,
+				},
+				StorageProfile: &compute.StorageProfile{
+					ImageReference: &compute.ImageReference{
+						Publisher: to.StringPtr(publisher),
+						Offer:     to.StringPtr(offer),
+						Sku:       to.StringPtr(sku),
+						Version:   to.StringPtr("latest"),
+					},
+					OsDisk: &compute.OSDisk{
+						Name: to.StringPtr("osDisk"),
+						Vhd: &compute.VirtualHardDisk{
+							URI: to.StringPtr(fmt.Sprintf(vhdURItemplate, storageAccountName, vmName)),
+						},
+						CreateOption: compute.FromImage,
+					},
+				},
+				OsProfile: &compute.OSProfile{
+					ComputerName:  to.StringPtr(vmName),
+					AdminUsername: to.StringPtr(username),
+					AdminPassword: to.StringPtr(password),
+					LinuxConfiguration: &compute.LinuxConfiguration{
+						SSH: &compute.SSHConfiguration{
+							PublicKeys: &[]compute.SSHPublicKey{
+								{
+									Path:    to.StringPtr(fmt.Sprintf("/home/%s/.ssh/authorized_keys", username)),
+									KeyData: to.StringPtr(sshKeyData),
+								},
+							},
+						},
+					},
+				},
+				NetworkProfile: &compute.NetworkProfile{
+					NetworkInterfaces: &[]compute.NetworkInterfaceReference{
+						{
+							ID: nic.ID,
+							NetworkInterfaceReferenceProperties: &compute.NetworkInterfaceReferenceProperties{
+								Primary: to.BoolPtr(true),
+							},
+						},
+					},
+				},
+			},
+		},
+	)
+	if err != nil {
+		return vm, fmt.Errorf("cannot create vm: %v", err)
+	}
+	err = future.WaitForCompletion(cntx, vmClient.Client)
+	if err != nil {
+		return vm, fmt.Errorf("cannot get the vm create or update future response: %v", err)
+	}
+	return future.Result(vmClient)
+}
---
+++ compute/hybrid/hybridcompute_test.go
@@ -0,0 +1,101 @@
+// Copyright (c) Microsoft and contributors.  All rights reserved.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package hybridcompute
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/Azure-Samples/azure-sdk-for-go-samples/helpers"
+	hybridnetwork "github.com/Azure-Samples/azure-sdk-for-go-samples/network/hybrid"
+	"github.com/Azure-Samples/azure-sdk-for-go-samples/resources"
+	hybridresources "github.com/Azure-Samples/azure-sdk-for-go-samples/resources/hybrid"
+	hybridstorage "github.com/Azure-Samples/azure-sdk-for-go-samples/storage/hybrid"
+)
+
+var (
+	vmName           = "az-samples-go-" + helpers.GetRandomLetterSequence(10)
+	nicName          = "nic1"
+	username         = "az-samples-go-user"
+	password         = "NoSoupForYou1!"
+	sshPublicKeyPath = os.Getenv("HOME") + "/.ssh/id_rsa.pub"
+
+	virtualNetworkName = "vnet1"
+	subnetName         = "subnet1"
+	nsgName            = "nsg1"
+	ipName             = "ip1"
+	storageAccountName = strings.ToLower("samplesa" + helpers.GetRandomLetterSequence(10))
+)
+
+func TestMain(m *testing.M) {
+	err := helpers.ParseArgs()
+	if err != nil {
+		log.Fatalln("failed to parse args")
+	}
+
+	ctx := context.Background()
+	defer resources.Cleanup(ctx)
+
+	_, err = hybridresources.CreateGroup(ctx)
+	if err != nil {
+		helpers.PrintAndLog(err.Error())
+	}
+	helpers.PrintAndLog(fmt.Sprintf("resource group created on location: %s", helpers.Location()))
+
+	os.Exit(m.Run())
+}
+
+func ExampleCreateVM() {
+	ctx := context.Background()
+
+	_, err := hybridnetwork.CreateVirtualNetworkAndSubnets(ctx, virtualNetworkName, subnetName)
+	if err != nil {
+		helpers.PrintAndLog(err.Error())
+	}
+	helpers.PrintAndLog("created vnet and a subnet")
+
+	_, err = hybridnetwork.CreateNetworkSecurityGroup(ctx, nsgName)
+	if err != nil {
+		helpers.PrintAndLog(err.Error())
+	}
+	helpers.PrintAndLog("created network security group")
+
+	_, err = hybridnetwork.CreatePublicIP(ctx, ipName)
+	if err != nil {
+		helpers.PrintAndLog(err.Error())
+	}
+	helpers.PrintAndLog("created public IP")
+
+	_, err = hybridnetwork.CreateNetworkInterface(ctx, nicName, nsgName, virtualNetworkName, subnetName, ipName)
+	if err != nil {
+		helpers.PrintAndLog(err.Error())
+	}
+	helpers.PrintAndLog("created nic")
+
+	_, err = hybridstorage.CreateStorageAccount(ctx, storageAccountName)
+	if err != nil {
+		helpers.PrintAndLog(err.Error())
+	}
+	helpers.PrintAndLog("created storage account")
+
+	_, err = CreateVM(ctx, vmName, nicName, username, password, storageAccountName, sshPublicKeyPath)
+	if err != nil {
+		helpers.PrintAndLog(err.Error())
+	}
+	helpers.PrintAndLog("created VM")
+
+	// Output:
+	// created vnet and a subnet
+	// created network security group
+	// created public IP
+	// created nic
+	// created storage account
+	// created VM
+}

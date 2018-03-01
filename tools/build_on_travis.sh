cebc9621225b4092be0c7b5f9ecd0661745efeac
Mariana Cardoso <mcardoso@microsoft.com>
2018-02-28 20:59:46 -0800

    Fix bash

--- tools/build_on_travis.sh
+++ tools/build_on_travis.sh
@@ -3,7 +3,7 @@ echo $TRAVIS_PULL_REQUEST
 echo $TRAVIS_BRANCH
 echo $TRAVIS_GO_VERSION
 
-if [ $TRAVIS_PULL_REQUEST != 'false' ] && [ $TRAVIS_BRANCH == 'master' ] && [ $TRAVIS_GO_VERSION == '1.10']; then
+if [ $TRAVIS_PULL_REQUEST != 'false' ] && [ $TRAVIS_BRANCH = 'master' ] && [ $TRAVIS_GO_VERSION = '1.10' ]; then
     echo 'Login to docker'
     docker login azureclidev.azurecr.io -p $AZURESDK_ACR_SP_PASSWORD -u $AZURESDKDEV_ACR_SP_USERNAME
     bash tools/build_container.sh

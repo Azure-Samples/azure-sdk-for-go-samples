33033cf29700e04e8185df281d75fc2f6f758197
Martin Strobel <marstr@microsoft.com>
2018-03-06 11:31:55 -0800

    Review Feedback

--- cdn/cdn_test.go
+++ cdn/cdn_test.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"fmt"
 	"log"
+
+	"github.com/Azure-Samples/azure-sdk-for-go-samples/helpers"
 )
 
 func ExampleCheckNameAvailability() {
 	ctx := context.Background()
 
-	available, err := CheckNameAvailability(ctx, "gocdnname", "Microsoft.Cdn/Profiles/Endpoints")
+	available, err := CheckNameAvailability(ctx, "gocdnname"+helpers.GetRandomLetterSequence(6), "Microsoft.Cdn/Profiles/Endpoints")
 	if err != nil {
 		log.Fatalf("cannot check availability: %v", err)
 	}

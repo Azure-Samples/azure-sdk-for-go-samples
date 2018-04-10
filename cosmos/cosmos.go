bc70aee1fe817d7db4827236e2a74adf42809e9d
Mariana Cardoso <mcardoso@microsoft.com>
2018-04-10 10:37:33 -0700

    Add cosmos DB sample (#166)
    * Add mongo DB sample
    
    * GO back to master branch on storage repo
    
    * Feedback

---
+++ cosmos/cosmos.go
@@ -0,0 +1,62 @@
+// Copyright (c) Microsoft and contributors.  All rights reserved.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package cosmos
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Azure-Samples/azure-sdk-for-go-samples/helpers"
+	"github.com/Azure-Samples/azure-sdk-for-go-samples/iam"
+	"github.com/Azure/azure-sdk-for-go/services/cosmos-db/mgmt/2015-04-08/documentdb"
+	"github.com/Azure/go-autorest/autorest/to"
+)
+
+func getDatabaseAccountClient() documentdb.DatabaseAccountsClient {
+	dbAccountClient := documentdb.NewDatabaseAccountsClient(helpers.SubscriptionID())
+	auth, _ := iam.GetResourceManagementAuthorizer(iam.AuthGrantType())
+	dbAccountClient.Authorizer = auth
+	dbAccountClient.AddToUserAgent(helpers.UserAgent())
+	return dbAccountClient
+}
+
+// CreateDatabaseAccount creates or updates an Azure Cosmos DB database account.
+func CreateDatabaseAccount(ctx context.Context, accountName string) (dba documentdb.DatabaseAccount, err error) {
+	dbAccountClient := getDatabaseAccountClient()
+	future, err := dbAccountClient.CreateOrUpdate(
+		ctx,
+		helpers.ResourceGroupName(),
+		accountName,
+		documentdb.DatabaseAccountCreateUpdateParameters{
+			Location: to.StringPtr(helpers.Location()),
+			Kind:     documentdb.GlobalDocumentDB,
+			DatabaseAccountCreateUpdateProperties: &documentdb.DatabaseAccountCreateUpdateProperties{
+				DatabaseAccountOfferType: to.StringPtr("Standard"),
+				Locations: &[]documentdb.Location{
+					{
+						FailoverPriority: to.Int32Ptr(0),
+						LocationName:     to.StringPtr(helpers.Location()),
+					},
+				},
+			},
+		})
+	if err != nil {
+		return dba, fmt.Errorf("cannot create database account: %v", err)
+	}
+
+	err = future.WaitForCompletion(ctx, dbAccountClient.Client)
+	if err != nil {
+		return dba, fmt.Errorf("cannot get the database account create or update future response: %v", err)
+	}
+
+	return future.Result(dbAccountClient)
+}
+
+// ListKeys gets the keys for a Azure Cosmos DB database account.
+func ListKeys(ctx context.Context, accountName string) (documentdb.DatabaseAccountListKeysResult, error) {
+	dbAccountClient := getDatabaseAccountClient()
+	return dbAccountClient.ListKeys(ctx, helpers.ResourceGroupName(), accountName)
+}
--- cosmos/cosmos_test.go
+++ cosmos/cosmos_test.go
@@ -4,3 +4,126 @@
 // LICENSE file in the root directory of this source tree.
 
 package cosmos
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/Azure-Samples/azure-sdk-for-go-samples/helpers"
+	"github.com/Azure-Samples/azure-sdk-for-go-samples/resources"
+	"github.com/Azure/azure-sdk-for-go/services/cosmos-db/mongodb"
+	"github.com/globalsign/mgo/bson"
+)
+
+var (
+	accountName = "cosmos-db-account-samples-" + helpers.GetRandomLetterSequence(10)
+)
+
+func TestMain(m *testing.M) {
+	flag.StringVar(&accountName, "cosmosDBAccountName", accountName, "Provide a name for the CosmosDB account to be created")
+
+	err := helpers.ParseArgs()
+	if err != nil {
+		log.Fatalln("failed to parse args")
+	}
+	os.Exit(m.Run())
+}
+
+func ExampleCosmosDBOperations() {
+	accountName := strings.ToLower(accountName)
+	helpers.SetResourceGroupName("CosmosDBOperations")
+	ctx := context.Background()
+	defer resources.Cleanup(ctx)
+	_, err := resources.CreateGroup(ctx, helpers.ResourceGroupName())
+	if err != nil {
+		helpers.PrintAndLog(err.Error())
+	}
+
+	_, err = CreateDatabaseAccount(ctx, accountName)
+	if err != nil {
+		helpers.PrintAndLog(fmt.Sprintf("cannot create database account: %v", err))
+	}
+	helpers.PrintAndLog("database account created")
+
+	keys, err := ListKeys(ctx, accountName)
+	if err != nil {
+		helpers.PrintAndLog(fmt.Sprintf("cannot list keys: %v", err))
+	}
+	helpers.PrintAndLog("keys listed")
+
+	host := fmt.Sprintf("%s.documents.azure.com", accountName)
+	collection := "Packages"
+
+	session, err := mongodb.NewMongoDBClientWithCredentials(accountName, *keys.PrimaryMasterKey, host)
+	if err != nil {
+		helpers.PrintAndLog(fmt.Sprintf("cannot get mongoDB session: %v", err))
+	}
+	helpers.PrintAndLog("got mongoDB session")
+
+	GetCollection(session, accountName, collection)
+	helpers.PrintAndLog("got collection")
+
+	err = InsertDocument(
+		session,
+		accountName,
+		collection,
+		map[string]interface{}{
+			"fullname":      "react",
+			"description":   "A framework for building native apps with React.",
+			"forksCount":    11392,
+			"StarsCount":    48794,
+			"LastUpdatedBy": "shergin",
+		})
+	if err != nil {
+		helpers.PrintAndLog(fmt.Sprintf("cannot insert document: %v", err))
+	}
+	helpers.PrintAndLog("inserted document")
+
+	doc, err := GetDocument(
+		session,
+		accountName,
+		collection,
+		bson.M{"fullname": "react"})
+	if err != nil {
+		helpers.PrintAndLog(fmt.Sprintf("cannot get document: %v", err))
+	}
+	helpers.PrintAndLog("got document")
+	helpers.PrintAndLog(fmt.Sprintf("document description: %s", doc["description"]))
+
+	err = UpdateDocument(
+		session,
+		accountName,
+		collection,
+		doc["_id"].(bson.ObjectId),
+		bson.M{
+			"$set": bson.M{
+				"fullname": "react-native",
+			},
+		})
+	if err != nil {
+		helpers.PrintAndLog(fmt.Sprintf("cannot update document: %v", err))
+	}
+	helpers.PrintAndLog("update document")
+
+	err = DeleteDcoument(session, accountName, collection, doc["_id"].(bson.ObjectId))
+	if err != nil {
+		helpers.PrintAndLog(fmt.Sprintf("cannot delete document: %v", err))
+	}
+	helpers.PrintAndLog("delete document")
+
+	// Output:
+	// database account created
+	// keys listed
+	// got mongoDB session
+	// got collection
+	// inserted document
+	// got document
+	// document description: A framework for building native apps with React.
+	// update document
+	// delete document
+}

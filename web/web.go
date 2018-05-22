201a6faec4ba152e7e98a8a67e9b95536d0474c4
Martin Strobel <marstr@microsoft.com>
2018-05-22 14:37:41 -0700

    fixing lint

--- web/web.go
+++ web/web.go
@@ -7,10 +7,10 @@ import (
 	"github.com/Azure-Samples/azure-sdk-for-go-samples/helpers"
 	"github.com/Azure-Samples/azure-sdk-for-go-samples/iam"
 	"github.com/Azure/azure-sdk-for-go/services/web/mgmt/2016-09-01/web"
-	"github.com/Azure/go-autorest/autorest"
 	"github.com/Azure/go-autorest/autorest/to"
 )
 
+// CreateContainerSite provisions all infrastructure needed to run an Azure Web App for Containers.
 func CreateContainerSite(ctx context.Context, name, image string) (createdConfig web.SiteConfigResource, err error) {
 	client := web.NewAppsClient(helpers.SubscriptionID())
 	client.Authorizer, err = iam.GetResourceManagementAuthorizer(iam.AuthGrantType())
@@ -32,15 +32,6 @@ func CreateContainerSite(ctx context.Context, name, image string) (createdConfig
 			},
 		})
 	if err != nil {
-		if cast, ok := err.(autorest.DetailedError); ok {
-			fmt.Println("Status: ", cast.Response.Status)
-			fmt.Println("Headers:")
-			for name, value := range cast.Response.Header {
-				fmt.Printf("\t%q -> %q\n", name, value)
-			}
-
-			fmt.Println("Decoded Body:\n\t", cast.Original)
-		}
 		return
 	}
 
@@ -50,35 +41,5 @@ func CreateContainerSite(ctx context.Context, name, image string) (createdConfig
 	}
 
 	createdConfig, err = client.GetConfiguration(ctx, helpers.ResourceGroupName(), name)
-	return
-}
-
-func CreateAppServicePlan(ctx context.Context, name, kind string) (created web.AppServicePlan, err error) {
-	client := web.NewAppServicePlansClient(helpers.SubscriptionID())
-	client.Authorizer, err = iam.GetResourceManagementAuthorizer(iam.AuthGrantType())
-	if err != nil {
-		return
-	}
-	client.AddToUserAgent(helpers.UserAgent())
-
-	future, err := client.CreateOrUpdate(
-		ctx,
-		helpers.ResourceGroupName(),
-		name,
-		web.AppServicePlan{
-			Location: to.StringPtr(helpers.Location()),
-			Kind:     &kind,
-		},
-	)
-	if err != nil {
-		return
-	}
-
-	err = future.WaitForCompletion(ctx, client.Client)
-	if err != nil {
-		return
-	}
-
-	created, err = future.Result(client)
 	return
 }

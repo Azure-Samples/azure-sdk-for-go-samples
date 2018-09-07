07a4591f09bdcc554ad8f4e860da6f4c4e0a511e
umar-muneer <umar.muneer@educative.io>
2018-09-07 12:17:48 +0500

    pre installing internal packages

---
+++ install.sh
@@ -0,0 +1,3 @@
+go install "github.com/Azure-Samples/azure-sdk-for-go-samples/compute"
+go install "github.com/Azure-Samples/azure-sdk-for-go-samples/internal/config"
+go install "github.com/Azure-Samples/azure-sdk-for-go-samples/internal/iam"

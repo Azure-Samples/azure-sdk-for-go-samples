b8a8e19073ea141acbc5880c305772ce64a891be
mayel <maespi@microsoft.com>
2020-06-16 03:04:14 +0000

    initial.

--- keyvault/keyvault_test.go
+++ keyvault/keyvault_test.go
@@ -6,76 +6,82 @@
 package keyvault
 
 import (
-	"context"
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/marstr/randname"
 
 	"github.com/Azure-Samples/azure-sdk-for-go-samples/internal/config"
-	"github.com/Azure-Samples/azure-sdk-for-go-samples/internal/util"
-	"github.com/Azure-Samples/azure-sdk-for-go-samples/resources"
 )
 
-var (
-	kvName  = randname.GenerateWithPrefix("vault-sample-go-", 5)
-	keyName = randname.GenerateWithPrefix("key-sample-go-", 5)
-)
+func addLocalEnvAndParse() error {
+	// parse env at top-level (also controls dotenv load)
+	err := config.ParseEnvironment()
+	if err != nil {
+		return fmt.Errorf("failed to add top-level env: %+v", err)
+	}
+
+	return nil
+}
+
+func addLocalFlagsAndParse() error {
+	// add top-level flags
+	err := config.AddFlags()
+	if err != nil {
+		return fmt.Errorf("failed to add top-level flags: %+v", err)
+	}
+
+	// parse all flags
+	flag.Parse()
+	return nil
+}
+
+func setup() error {
+	var err error
+	err = addLocalEnvAndParse()
+	if err != nil {
+		return err
+	}
+
+	err = addLocalFlagsAndParse()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func teardown() error {
+	return nil
+}
+
+// test helpers
+func generateName(prefix string) string {
+	return strings.ToLower(randname.GenerateWithPrefix(prefix, 5))
+}
 
 // TestMain sets up the environment and initiates tests.
 func TestMain(m *testing.M) {
 	var err error
-	err = config.ParseEnvironment()
+	var code int
+
+	err = setup()
 	if err != nil {
-		log.Fatalf("failed to parse env: %+v", err)
+		log.Fatalf("could not set up environment: %+v", err)
 	}
 
-	err = config.AddFlags()
-	if err != nil {
-		log.Fatalf("failed to parse flags: %+v", err)
-	}
-	flag.Parse()
+	code = m.Run()
 
-	code := m.Run()
+	err = teardown()
+	if err != nil {
+		log.Fatalf(
+			"could not tear down environment: %v\n; original exit code: %v\n",
+			err, code)
+	}
+
 	os.Exit(code)
-}
-
-func ExampleSetVaultPermissions() {
-	var groupName = config.GenerateGroupName("KeyVault")
-	config.SetGroupName(groupName)
-
-	ctx := context.Background()
-	defer resources.Cleanup(ctx)
-
-	_, err := resources.CreateGroup(ctx, config.GroupName())
-	if err != nil {
-		util.LogAndPanic(err)
-	}
-	util.PrintAndLog("resource group created")
-
-	_, err = CreateVault(ctx, kvName)
-	if err != nil {
-		util.LogAndPanic(err)
-	}
-	util.PrintAndLog("vault created")
-
-	_, err = SetVaultPermissions(ctx, kvName)
-	if err != nil {
-		util.LogAndPanic(err)
-	}
-	util.PrintAndLog("set vault permissions")
-
-	_, err = CreateKey(ctx, kvName, keyName)
-	if err != nil {
-		util.LogAndPanic(err)
-	}
-	util.PrintAndLog("created key")
-
-	// Output:
-	// resource group created
-	// vault created
-	// set vault permissions
-	// created key
 }

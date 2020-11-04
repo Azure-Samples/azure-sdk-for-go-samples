812b19947704931ec85a2cb9f3ac6640e8428abe
Georgios Christoglou <gechris@microsoft.com>
2020-11-03 22:55:48 -0800

    Add the mysql samples

--- mysql/mysql_client.go
+++ mysql/mysql_client.go
@@ -12,7 +12,7 @@ import (
 )
 
 // GetServersClient returns
-func GetServersClient() mysql.ServersClient {
+func getServersClient() mysql.ServersClient {
 	serversClient := mysql.NewServersClient(config.SubscriptionID())
 	a, _ := iam.GetResourceManagementAuthorizer()
 	serversClient.Authorizer = a
@@ -20,8 +20,9 @@ func GetServersClient() mysql.ServersClient {
 	return serversClient
 }
 
-// CreateServer creates a new PostgreSQL Server
-func CreateServer(ctx context.Context, serversClient mysql.ServersClient, serverName string, dbLogin string, dbPassword string) (server mysql.Server, err error) {
+// CreateServer creates a new MySQL Server
+func CreateServer(ctx context.Context, serverName, dbLogin, dbPassword string) (server mysql.Server, err error) {
+	serversClient := getServersClient()
 
 	// Create the server
 	future, err := serversClient.Create(
@@ -31,7 +32,7 @@ func CreateServer(ctx context.Context, serversClient mysql.ServersClient, server
 		mysql.Server{
 			Location: to.StringPtr(config.Location()),
 			Sku: &mysql.Sku{
-				Name: to.StringPtr("Standard_D4s_v3"),
+				Name: to.StringPtr("Standard_D16ds_v4"),
 				Tier: "GeneralPurpose",
 			},
 			ServerProperties: &mysql.ServerProperties{
@@ -57,7 +58,8 @@ func CreateServer(ctx context.Context, serversClient mysql.ServersClient, server
 }
 
 // UpdateServerStorageCapacity given the server name and the new storage capacity it updates the server's storage capacity.
-func UpdateServerStorageCapacity(ctx context.Context, serversClient mysql.ServersClient, serverName string, storageCapacity int32) (server mysql.Server, err error) {
+func UpdateServerStorageCapacity(ctx context.Context, serverName string, storageCapacity int32) (server mysql.Server, err error) {
+	serversClient := getServersClient()
 
 	future, err := serversClient.Update(
 		ctx,
@@ -83,8 +85,9 @@ func UpdateServerStorageCapacity(ctx context.Context, serversClient mysql.Server
 	return future.Result(serversClient)
 }
 
-// DeleteServer deletes the PostgreSQL server.
-func DeleteServer(ctx context.Context, serversClient mysql.ServersClient, serverName string) (resp autorest.Response, err error) {
+// DeleteServer deletes the MySQL server.
+func DeleteServer(ctx context.Context, serverName string) (resp autorest.Response, err error) {
+	serversClient := getServersClient()
 
 	future, err := serversClient.Delete(ctx, config.GroupName(), serverName)
 	if err != nil {
@@ -100,7 +103,7 @@ func DeleteServer(ctx context.Context, serversClient mysql.ServersClient, server
 }
 
 // GetFwRulesClient returns the FirewallClient
-func GetFwRulesClient() mysql.FirewallRulesClient {
+func getFwRulesClient() mysql.FirewallRulesClient {
 	fwrClient := mysql.NewFirewallRulesClient(config.SubscriptionID())
 	a, _ := iam.GetResourceManagementAuthorizer()
 	fwrClient.Authorizer = a
@@ -109,7 +112,8 @@ func GetFwRulesClient() mysql.FirewallRulesClient {
 }
 
 // CreateOrUpdateFirewallRule given the firewallname and new properties it updates the firewall rule.
-func CreateOrUpdateFirewallRule(ctx context.Context, fwrClient mysql.FirewallRulesClient, serverName, firewallRuleName, startIPAddr, endIPAddr string) error {
+func CreateOrUpdateFirewallRule(ctx context.Context, serverName, firewallRuleName, startIPAddr, endIPAddr string) error {
+	fwrClient := getFwRulesClient()
 
 	_, err := fwrClient.CreateOrUpdate(
 		ctx,
@@ -128,7 +132,7 @@ func CreateOrUpdateFirewallRule(ctx context.Context, fwrClient mysql.FirewallRul
 }
 
 // GetConfigurationsClient creates and returns the configuration client for the server.
-func GetConfigurationsClient() mysql.ConfigurationsClient {
+func getConfigurationsClient() mysql.ConfigurationsClient {
 	configClient := mysql.NewConfigurationsClient(config.SubscriptionID())
 	a, _ := iam.GetResourceManagementAuthorizer()
 	configClient.Authorizer = a
@@ -137,7 +141,8 @@ func GetConfigurationsClient() mysql.ConfigurationsClient {
 }
 
 // GetConfiguration given the server name and configuration name it returns the configuration.
-func GetConfiguration(ctx context.Context, configClient mysql.ConfigurationsClient, serverName string, configurationName string) (mysql.Configuration, error) {
+func GetConfiguration(ctx context.Context, serverName, configurationName string) (mysql.Configuration, error) {
+	configClient := getConfigurationsClient()
 
 	// Get the configuration.
 	configuration, err := configClient.Get(ctx, config.GroupName(), serverName, configurationName)
@@ -150,7 +155,8 @@ func GetConfiguration(ctx context.Context, configClient mysql.ConfigurationsClie
 }
 
 // UpdateConfiguration given the name of the configuation and the configuration object it updates the configuration for the given server.
-func UpdateConfiguration(ctx context.Context, configClient mysql.ConfigurationsClient, serverName string, configurationName string, configuration mysql.Configuration) (updatedConfig mysql.Configuration, err error) {
+func UpdateConfiguration(ctx context.Context, serverName string, configurationName string, configuration mysql.Configuration) (updatedConfig mysql.Configuration, err error) {
+	configClient := getConfigurationsClient()
 
 	future, err := configClient.Update(ctx, config.GroupName(), serverName, configurationName, configuration)
 
--- mysql/mysql_client_test.go
+++ mysql/mysql_client_test.go
@@ -18,10 +18,11 @@ import (
 )
 
 var (
+	groupName  = config.GenerateGroupName("MysqlServerOperations")
 	serverName = generateName("gosdkmysql")
 	dbName     = "mysqldb1"
 	dbLogin    = "mysqldbuser1"
-	dbPassword = "mysqldbuserpass!1"
+	dbPassword = generatePassword("mysqldbuserpass!1")
 )
 
 func addLocalEnvAndParse() error {
@@ -40,10 +41,10 @@ func addLocalFlagsAndParse() error {
 		return fmt.Errorf("failed to add top-level flags: %+v", err)
 	}
 
-	flag.StringVar(&serverName, "sqlServerName", serverName, "Name for SQL server.")
-	flag.StringVar(&dbName, "sqlDbName", dbName, "Name for SQL database.")
-	flag.StringVar(&dbLogin, "sqlDbUsername", dbLogin, "Username for SQL login.")
-	flag.StringVar(&dbPassword, "sqlDbPassword", dbPassword, "Password for SQL login.")
+	flag.StringVar(&serverName, "sqlServerName", serverName, "Name for MySQL server.")
+	flag.StringVar(&dbName, "sqlDbName", dbName, "Name for MySQL database.")
+	flag.StringVar(&dbLogin, "sqlDbUsername", dbLogin, "Username for MySQL login.")
+	flag.StringVar(&dbPassword, "sqlDbPassword", dbPassword, "Password for MySQL login.")
 
 	// parse all flags
 	flag.Parse()
@@ -78,6 +79,11 @@ func teardown() error {
 // test helpers
 func generateName(prefix string) string {
 	return strings.ToLower(randname.GenerateWithPrefix(prefix, 5))
+}
+
+// Just add 5 random digits at the end of the prefix password.
+func generatePassword(pass string) string {
+	return randname.GenerateWithPrefix(pass, 5)
 }
 
 // TestMain sets up the environment and initiates tests.
@@ -117,39 +123,33 @@ func Example_performServerOperations() {
 		util.LogAndPanic(err)
 	}
 
-	serversClient := GetServersClient()
-
-	_, err = CreateServer(ctx, serversClient, serverName, dbLogin, dbPassword)
+	_, err = CreateServer(ctx, serverName, dbLogin, dbPassword)
 	if err != nil {
 		util.LogAndPanic(fmt.Errorf("cannot create mysql server: %+v", err))
 	}
 	util.PrintAndLog("mysql server created")
 
-	_, err = UpdateServerStorageCapacity(ctx, serversClient, serverName, 1048576)
+	_, err = UpdateServerStorageCapacity(ctx, serverName, 1048576)
 	if err != nil {
 		util.LogAndPanic(fmt.Errorf("cannot update mysql server: %+v", err))
 	}
 	util.PrintAndLog("updated mysql server's storage capacity")
 
-	fwrClient := GetFwRulesClient()
-
-	err = CreateOrUpdateFirewallRule(ctx, fwrClient, serverName, "FirewallRuleName", "0.0.0.0", "0.0.0.0")
+	err = CreateOrUpdateFirewallRule(ctx, serverName, "FirewallRuleName", "0.0.0.0", "0.0.0.0")
 	if err != nil {
 		util.LogAndPanic(err)
 	}
 	util.PrintAndLog("Firewall rule set")
 
-	err = CreateOrUpdateFirewallRule(ctx, fwrClient, serverName, "FirewallRuleName", "0.0.0.0", "1.1.1.1")
+	err = CreateOrUpdateFirewallRule(ctx, serverName, "FirewallRuleName", "0.0.0.0", "1.1.1.1")
 	if err != nil {
 		util.LogAndPanic(err)
 	}
 	util.PrintAndLog("Firewall rule updated")
 
-	configClient := GetConfigurationsClient()
-
 	var configuration mysql.Configuration
 
-	configuration, err = GetConfiguration(ctx, configClient, serverName, "event_scheduler")
+	configuration, err = GetConfiguration(ctx, serverName, "event_scheduler")
 	if err != nil {
 		util.LogAndPanic(err)
 	}
@@ -157,17 +157,27 @@ func Example_performServerOperations() {
 
 	// Update the configuration Value.
 	configuration.ConfigurationProperties.Value = to.StringPtr("on")
+	configuration.ConfigurationProperties.Source = to.StringPtr("user-override")
 
-	_, err = UpdateConfiguration(ctx, configClient, serverName, "event_scheduler", configuration)
+	_, err = UpdateConfiguration(ctx, serverName, "event_scheduler", configuration)
 	if err != nil {
 		util.LogAndPanic(err)
 	}
 	util.PrintAndLog("Updated the event_scheduler configuration")
 
 	// Finally delete the server.
-	_, err = DeleteServer(ctx, serversClient, serverName)
+	_, err = DeleteServer(ctx, serverName)
 	if err != nil {
 		util.LogAndPanic(err)
 	}
 	util.PrintAndLog("Successfully deleted the server")
+
+	// Output:
+	// mysql server created
+	// updated mysql server's storage capacity
+	// Firewall rule set
+	// Firewall rule updated
+	// Got the event_scheduler configuration
+	// Updated the event_scheduler configuration
+	// Successfully deleted the server
 }

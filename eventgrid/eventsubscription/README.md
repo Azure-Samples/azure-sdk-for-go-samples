84325a64bd84500a97565901f7020d3a48abe980
804873052 <804873052@qq.com>
2021-10-20 14:28:11 +0800

    other

---
+++ eventgrid/eventsubscription/README.md
@@ -0,0 +1,63 @@
+---
+page_type: sample
+languages:
+- golang
+products:
+- azure
+description: "These code samples will show you how to manage Event Grid using Azure SDK for Golang."
+urlFragment: eventgrid
+---
+
+# Getting started - Managing Event Grid using Azure Golang SDK
+
+These code samples will show you how to manage Event Grid using Azure SDK for Golang.
+
+## Features
+
+This project framework provides examples for the following services:
+
+### Event Grid
+* Using the Azure SDK for Golang - Event Grid Library [eventgrid/armeventgrid](https://pkg.go.dev/github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/eventgrid/armeventgrid) for the [Azure Event Grid API](https://docs.microsoft.com/en-us/rest/api/eventgrid/)
+
+### Prerequisites
+* an [Azure subscription](https://azure.microsoft.com)
+* Go 1.13 or above
+
+### Quickstart
+
+1. Clone the repository.
+
+    ```
+    git clone https://github.com/Azure-Samples/azure-sdk-for-go-samples.git --branch new-version
+    ```
+   
+2. Set the environment variable.
+
+   ```
+   # bash
+   export AZURE_SUBSCRIPTION_ID=<your Azure subscription id> 
+   # If no value is set, the created resource will be deleted by default.
+   # anything other than empty to keep the resources
+   export KEEP_RESOURCE=1 
+   export AZURE_TENANT_ID=<your Azure Tenant id>          
+   export AZURE_OBJECT_ID=<your Azure Client/Object id> 
+   ```
+
+3. Run eventgrid sample.
+
+    ```
+    cd azure-sdk-for-go-samples/eventgrid/eventsubscription
+    go mod tidy
+    go run main.go
+    ```
+   
+## Resources
+
+- https://github.com/Azure/azure-sdk-for-go
+- https://docs.microsoft.com/en-us/azure/developer/go/
+- https://docs.microsoft.com/en-us/rest/api/
+- https://pkg.go.dev/github.com/Azure/azure-sdk-for-go/sdk
+
+## Need help?
+
+Post issue on Github (https://github.com/Azure/azure-sdk-for-go/issues)
---
+++ eventgrid/partnerregistration/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/eventgrid/armeventgrid"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
+)
+
+var (
+	subscriptionID          string
+	location                = "westus"
+	resourceGroupName       = "sample-resource-group"
+	partnerRegistrationName = "sample-partner-registration"
+)
+
+func main() {
+	subscriptionID = os.Getenv("AZURE_SUBSCRIPTION_ID")
+	if len(subscriptionID) == 0 {
+		log.Fatal("AZURE_SUBSCRIPTION_ID is not set.")
+	}
+
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conn := arm.NewDefaultConnection(cred, &arm.ConnectionOptions{
+		Logging: policy.LogOptions{
+			IncludeBody: true,
+		},
+	})
+	ctx := context.Background()
+
+	resourceGroup, err := createResourceGroup(ctx, conn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("resources group:", *resourceGroup.ID)
+
+	partnerRegistration, err := createPartnerRegistration(ctx, conn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("partner registration:", *partnerRegistration.ID)
+
+	keepResource := os.Getenv("KEEP_RESOURCE")
+	if len(keepResource) == 0 {
+		_, err := cleanup(ctx, conn)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("cleaned up successfully.")
+	}
+}
+
+func createPartnerRegistration(ctx context.Context, conn *arm.Connection) (*armeventgrid.PartnerRegistration, error) {
+	partnerRegistrationsClient := armeventgrid.NewPartnerRegistrationsClient(conn, subscriptionID)
+
+	resp, err := partnerRegistrationsClient.CreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		partnerRegistrationName,
+		armeventgrid.PartnerRegistration{
+			TrackedResource: armeventgrid.TrackedResource{
+				Location: to.StringPtr(location),
+			},
+		},
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &resp.PartnerRegistration, nil
+}
+
+func createResourceGroup(ctx context.Context, conn *arm.Connection) (*armresources.ResourceGroup, error) {
+	resourceGroupClient := armresources.NewResourceGroupsClient(conn, subscriptionID)
+
+	resourceGroupResp, err := resourceGroupClient.CreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		armresources.ResourceGroup{
+			Location: to.StringPtr(location),
+		},
+		nil)
+	if err != nil {
+		return nil, err
+	}
+	return &resourceGroupResp.ResourceGroup, nil
+}
+
+func cleanup(ctx context.Context, conn *arm.Connection) (*http.Response, error) {
+	resourceGroupClient := armresources.NewResourceGroupsClient(conn, subscriptionID)
+
+	pollerResp, err := resourceGroupClient.BeginDelete(ctx, resourceGroupName, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := pollerResp.PollUntilDone(ctx, 10*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	return resp.RawResponse, nil
+}

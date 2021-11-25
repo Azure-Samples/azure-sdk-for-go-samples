18586ed1fa241e53c3137242055f4aeb664d4710
Jiahui Peng <46921893+804873052@users.noreply.github.com>
2021-11-25 15:26:37 +0800

    Add apimanagement sample (#322)
    * Add apimanagement sample
    
    * Add apimanagement sample
    
    * refresh

---
+++ apimanagement/apioperation/main.go
@@ -0,0 +1,196 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/apimanagement/armapimanagement"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
+)
+
+var (
+	subscriptionID    string
+	location          = "westus"
+	resourceGroupName = "sample-resource-group"
+	serviceName       = "sample-api-service"
+	apiID             = "sample-api"
+	operationID       = "sample-api-operation"
+)
+
+func main() {
+	subscriptionID = os.Getenv("AZURE_SUBSCRIPTION_ID")
+	if len(subscriptionID) == 0 {
+		log.Fatal("AZURE_SUBSCRIPTION_ID is not set.")
+	}
+
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+
+	resourceGroup, err := createResourceGroup(ctx, cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("resources group:", *resourceGroup.ID)
+
+	apiManagementService, err := createApiManagementService(ctx, cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("api management service:", *apiManagementService.ID)
+
+	api, err := createApi(ctx, cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("api:", *api.ID)
+
+	apiOperation, err := createApiOperation(ctx, cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("api operation:", *apiOperation.ID)
+
+	keepResource := os.Getenv("KEEP_RESOURCE")
+	if len(keepResource) == 0 {
+		_, err := cleanup(ctx, cred)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("cleaned up successfully.")
+	}
+}
+
+func createApiManagementService(ctx context.Context, cred azcore.TokenCredential) (*armapimanagement.APIManagementServiceResource, error) {
+	apiManagementServiceClient := armapimanagement.NewAPIManagementServiceClient(subscriptionID, cred, nil)
+
+	pollerResp, err := apiManagementServiceClient.BeginCreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		serviceName,
+		armapimanagement.APIManagementServiceResource{
+			Location: to.StringPtr(location),
+			Properties: &armapimanagement.APIManagementServiceProperties{
+				PublisherName:  to.StringPtr("sample"),
+				PublisherEmail: to.StringPtr("xxx@wircesoft.com"),
+			},
+			SKU: &armapimanagement.APIManagementServiceSKUProperties{
+				Name:     armapimanagement.SKUTypeStandard.ToPtr(),
+				Capacity: to.Int32Ptr(2),
+			},
+		},
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := pollerResp.PollUntilDone(ctx, 10*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	return &resp.APIManagementServiceResource, nil
+}
+
+func createApi(ctx context.Context, cred azcore.TokenCredential) (*armapimanagement.APIContract, error) {
+	APIClient := armapimanagement.NewAPIClient(subscriptionID, cred, nil)
+
+	pollerResp, err := APIClient.BeginCreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		serviceName,
+		apiID,
+		armapimanagement.APICreateOrUpdateParameter{
+			Properties: &armapimanagement.APICreateOrUpdateProperties{
+				APIContractProperties: armapimanagement.APIContractProperties{
+					Path:        to.StringPtr("test"),
+					DisplayName: to.StringPtr("sample-sample"),
+					Protocols: []*armapimanagement.Protocol{
+						armapimanagement.ProtocolHTTP.ToPtr(),
+						armapimanagement.ProtocolHTTPS.ToPtr(),
+					},
+				},
+			},
+		},
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := pollerResp.PollUntilDone(ctx, 10*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	return &resp.APIContract, nil
+}
+
+func createApiOperation(ctx context.Context, cred azcore.TokenCredential) (*armapimanagement.OperationContract, error) {
+	apiOperationClient := armapimanagement.NewAPIOperationClient(subscriptionID, cred, nil)
+
+	resp, err := apiOperationClient.CreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		serviceName,
+		apiID,
+		operationID,
+		armapimanagement.OperationContract{
+			Properties: &armapimanagement.OperationContractProperties{
+				DisplayName: to.StringPtr("sample operation"),
+				Method:      to.StringPtr("GET"),
+				URLTemplate: to.StringPtr("/operation/customers/{uid}"),
+				OperationEntityBaseContract: armapimanagement.OperationEntityBaseContract{
+					TemplateParameters: []*armapimanagement.ParameterContract{
+						{
+							Name:        to.StringPtr("uid"),
+							Type:        to.StringPtr("string"),
+							Description: to.StringPtr("user id"),
+						},
+					},
+				},
+			},
+		},
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &resp.OperationContract, nil
+}
+
+func createResourceGroup(ctx context.Context, cred azcore.TokenCredential) (*armresources.ResourceGroup, error) {
+	resourceGroupClient := armresources.NewResourceGroupsClient(subscriptionID, cred, nil)
+
+	resourceGroupResp, err := resourceGroupClient.CreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		armresources.ResourceGroup{
+			Location: to.StringPtr(location),
+		},
+		nil)
+	if err != nil {
+		return nil, err
+	}
+	return &resourceGroupResp.ResourceGroup, nil
+}
+
+func cleanup(ctx context.Context, cred azcore.TokenCredential) (*http.Response, error) {
+	resourceGroupClient := armresources.NewResourceGroupsClient(subscriptionID, cred, nil)
+
+	pollerResp, err := resourceGroupClient.BeginDelete(ctx, resourceGroupName, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := pollerResp.PollUntilDone(ctx, 10*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	return resp.RawResponse, nil
+}
---
+++ apimanagement/logger/main.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/apimanagement/armapimanagement"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
+)
+
+var (
+	subscriptionID    string
+	location          = "westus"
+	resourceGroupName = "sample-resource-group"
+	serviceName       = "sample-api-service"
+	loggerID          = "sample2logger"
+)
+
+func main() {
+	subscriptionID = os.Getenv("AZURE_SUBSCRIPTION_ID")
+	if len(subscriptionID) == 0 {
+		log.Fatal("AZURE_SUBSCRIPTION_ID is not set.")
+	}
+
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+
+	resourceGroup, err := createResourceGroup(ctx, cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("resources group:", *resourceGroup.ID)
+
+	apiManagementService, err := createApiManagementService(ctx, cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("api management service:", *apiManagementService.ID)
+
+	apiLogger, err := createLogger(ctx, cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("api logger:", *apiLogger.ID)
+
+	keepResource := os.Getenv("KEEP_RESOURCE")
+	if len(keepResource) == 0 {
+		_, err := cleanup(ctx, cred)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("cleaned up successfully.")
+	}
+}
+
+func createApiManagementService(ctx context.Context, cred azcore.TokenCredential) (*armapimanagement.APIManagementServiceResource, error) {
+	apiManagementServiceClient := armapimanagement.NewAPIManagementServiceClient(subscriptionID, cred, nil)
+
+	pollerResp, err := apiManagementServiceClient.BeginCreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		serviceName,
+		armapimanagement.APIManagementServiceResource{
+			Location: to.StringPtr(location),
+			Properties: &armapimanagement.APIManagementServiceProperties{
+				PublisherName:  to.StringPtr("sample"),
+				PublisherEmail: to.StringPtr("xxx@wircesoft.com"),
+			},
+			SKU: &armapimanagement.APIManagementServiceSKUProperties{
+				Name:     armapimanagement.SKUTypeStandard.ToPtr(),
+				Capacity: to.Int32Ptr(1),
+			},
+		},
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := pollerResp.PollUntilDone(ctx, 10*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	return &resp.APIManagementServiceResource, nil
+}
+
+func createLogger(ctx context.Context, cred azcore.TokenCredential) (*armapimanagement.LoggerContract, error) {
+	loggerClient := armapimanagement.NewLoggerClient(subscriptionID, cred, nil)
+
+	resp, err := loggerClient.CreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		serviceName,
+		loggerID,
+		armapimanagement.LoggerContract{
+			Properties: &armapimanagement.LoggerContractProperties{
+				LoggerType:  armapimanagement.LoggerTypeApplicationInsights.ToPtr(),
+				Description: to.StringPtr("sample api logger"),
+				Credentials: map[string]*string{
+					"credectionString": to.StringPtr("key1=value1;InstrumentationKey=00000000-0000-0000-0000-000000000000"),
+				},
+			},
+		},
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &resp.LoggerContract, nil
+}
+
+func createResourceGroup(ctx context.Context, cred azcore.TokenCredential) (*armresources.ResourceGroup, error) {
+	resourceGroupClient := armresources.NewResourceGroupsClient(subscriptionID, cred, nil)
+
+	resourceGroupResp, err := resourceGroupClient.CreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		armresources.ResourceGroup{
+			Location: to.StringPtr(location),
+		},
+		nil)
+	if err != nil {
+		return nil, err
+	}
+	return &resourceGroupResp.ResourceGroup, nil
+}
+
+func cleanup(ctx context.Context, cred azcore.TokenCredential) (*http.Response, error) {
+	resourceGroupClient := armresources.NewResourceGroupsClient(subscriptionID, cred, nil)
+
+	pollerResp, err := resourceGroupClient.BeginDelete(ctx, resourceGroupName, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := pollerResp.PollUntilDone(ctx, 10*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	return resp.RawResponse, nil
+}

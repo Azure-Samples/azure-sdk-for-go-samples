6d83838692643433d3a7c38addf053a6b98e68a7
Jiahui Peng <46921893+804873052@users.noreply.github.com>
2021-11-26 10:43:35 +0800

    Add sql sample (#315)
    * sample
    
    * running
    
    * hava error
    
    * refresh,but need run test

---
+++ sql/server_key/main.go
@@ -0,0 +1,277 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/keyvault/armkeyvault"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/sql/armsql"
+)
+
+var (
+	subscriptionID    string
+	TenantID          string
+	ObjectID          string
+	location          = "westus"
+	resourceGroupName = "sample-resource-group"
+	serverName        = "sample2server"
+	vaultName         = "sample2vault223"
+	keyName           = "sample2key223"
+	serverKeyName     = "sample-server-key"
+)
+
+func main() {
+	subscriptionID = os.Getenv("AZURE_SUBSCRIPTION_ID")
+	if len(subscriptionID) == 0 {
+		log.Fatal("AZURE_SUBSCRIPTION_ID is not set.")
+	}
+
+	TenantID = os.Getenv("AZURE_TENANT_ID")
+	if len(TenantID) == 0 {
+		log.Fatal("AZURE_TENANT_ID is not set.")
+	}
+
+	ObjectID = os.Getenv("AZURE_OBJECT_ID")
+	if len(ObjectID) == 0 {
+		log.Fatal("AZURE_OBJECT_ID is not set.")
+	}
+
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+
+	resourceGroup, err := createResourceGroup(ctx, cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("resources group:", *resourceGroup.ID)
+
+	server, err := createServer(ctx, cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("server:", *server.ID)
+
+	vault, err := createVault(ctx, cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("vault:", *vault.ID)
+
+	key, err := createKey(ctx, cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("key:", *key.ID)
+
+	serverKey, err := createServerKey(ctx, cred, *key.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("server key:", *serverKey.ID)
+
+	keepResource := os.Getenv("KEEP_RESOURCE")
+	if len(keepResource) == 0 {
+		_, err := cleanup(ctx, cred)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("cleaned up successfully.")
+	}
+}
+
+func createServer(ctx context.Context, cred azcore.TokenCredential) (*armsql.Server, error) {
+	serversClient := armsql.NewServersClient(subscriptionID, cred, nil)
+
+	pollerResp, err := serversClient.BeginCreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		serverName,
+		armsql.Server{
+			TrackedResource: armsql.TrackedResource{
+				Location: to.StringPtr(location),
+			},
+			Identity: &armsql.ResourceIdentity{
+				Type: armsql.IdentityTypeSystemAssigned.ToPtr(),
+			},
+			Properties: &armsql.ServerProperties{
+				AdministratorLogin:         to.StringPtr("dummylogin"),
+				AdministratorLoginPassword: to.StringPtr("QWE123!@#"),
+				Version:                    to.StringPtr("12.0"),
+				PublicNetworkAccess:        armsql.ServerNetworkAccessFlagEnabled.ToPtr(),
+			},
+		},
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := pollerResp.PollUntilDone(ctx, 10*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	return &resp.Server, nil
+}
+
+func createVault(ctx context.Context, cred azcore.TokenCredential) (*armkeyvault.Vault, error) {
+	vaultsClient := armkeyvault.NewVaultsClient(subscriptionID, cred, nil)
+
+	pollerResp, err := vaultsClient.BeginCreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		vaultName,
+		armkeyvault.VaultCreateOrUpdateParameters{
+			Location: to.StringPtr(location),
+			Properties: &armkeyvault.VaultProperties{
+				SKU: &armkeyvault.SKU{
+					Family: armkeyvault.SKUFamilyA.ToPtr(),
+					Name:   armkeyvault.SKUNameStandard.ToPtr(),
+				},
+				TenantID: to.StringPtr(TenantID),
+				AccessPolicies: []*armkeyvault.AccessPolicyEntry{
+					{
+						TenantID: to.StringPtr(TenantID),
+						ObjectID: to.StringPtr(ObjectID),
+						Permissions: &armkeyvault.Permissions{
+							Keys: []*armkeyvault.KeyPermissions{
+								armkeyvault.KeyPermissionsGet.ToPtr(),
+								armkeyvault.KeyPermissionsList.ToPtr(),
+								armkeyvault.KeyPermissionsCreate.ToPtr(),
+							},
+							Secrets: []*armkeyvault.SecretPermissions{
+								armkeyvault.SecretPermissionsGet.ToPtr(),
+								armkeyvault.SecretPermissionsList.ToPtr(),
+							},
+							Certificates: []*armkeyvault.CertificatePermissions{
+								armkeyvault.CertificatePermissionsGet.ToPtr(),
+								armkeyvault.CertificatePermissionsList.ToPtr(),
+								armkeyvault.CertificatePermissionsCreate.ToPtr(),
+							},
+							Storage: []*armkeyvault.StoragePermissions{
+								armkeyvault.StoragePermissionsGet.ToPtr(),
+								armkeyvault.StoragePermissionsList.ToPtr(),
+								armkeyvault.StoragePermissionsDelete.ToPtr(),
+								armkeyvault.StoragePermissionsSet.ToPtr(),
+							},
+						},
+					},
+				},
+				EnabledForDiskEncryption:  to.BoolPtr(true),
+				EnableSoftDelete:          to.BoolPtr(true),
+				SoftDeleteRetentionInDays: to.Int32Ptr(90),
+				NetworkACLs: &armkeyvault.NetworkRuleSet{
+					Bypass:              armkeyvault.NetworkRuleBypassOptionsAzureServices.ToPtr(),
+					DefaultAction:       armkeyvault.NetworkRuleActionAllow.ToPtr(),
+					IPRules:             []*armkeyvault.IPRule{},
+					VirtualNetworkRules: []*armkeyvault.VirtualNetworkRule{},
+				},
+			},
+		},
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := pollerResp.PollUntilDone(ctx, 10*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	return &resp.Vault, nil
+}
+
+func createKey(ctx context.Context, cred azcore.TokenCredential) (*armkeyvault.Key, error) {
+	keysClient := armkeyvault.NewKeysClient(subscriptionID, cred, nil)
+
+	secretResp, err := keysClient.CreateIfNotExist(
+		ctx,
+		resourceGroupName,
+		vaultName,
+		keyName,
+		armkeyvault.KeyCreateParameters{
+			Properties: &armkeyvault.KeyProperties{
+				Attributes: &armkeyvault.KeyAttributes{
+					Enabled: to.BoolPtr(true),
+				},
+				KeySize: to.Int32Ptr(2048),
+				KeyOps: []*armkeyvault.JSONWebKeyOperation{
+					armkeyvault.JSONWebKeyOperationEncrypt.ToPtr(),
+					armkeyvault.JSONWebKeyOperationDecrypt.ToPtr(),
+				},
+				Kty: armkeyvault.JSONWebKeyTypeRSA.ToPtr(),
+			},
+		},
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &secretResp.Key, nil
+}
+
+func createServerKey(ctx context.Context, cred azcore.TokenCredential, keyID string) (*armsql.ServerKey, error) {
+	serverKeysClient := armsql.NewServerKeysClient(subscriptionID, cred, nil)
+
+	pollerResp, err := serverKeysClient.BeginCreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		serverName,
+		serverKeyName,
+		armsql.ServerKey{
+			Properties: &armsql.ServerKeyProperties{
+				ServerKeyType: armsql.ServerKeyTypeAzureKeyVault.ToPtr(),
+				URI:           to.StringPtr(keyID),
+			},
+		},
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := pollerResp.PollUntilDone(ctx, 10*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	return &resp.ServerKey, nil
+}
+
+func createResourceGroup(ctx context.Context, cred azcore.TokenCredential) (*armresources.ResourceGroup, error) {
+	resourceGroupClient := armresources.NewResourceGroupsClient(subscriptionID, cred, nil)
+
+	resourceGroupResp, err := resourceGroupClient.CreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		armresources.ResourceGroup{
+			Location: to.StringPtr(location),
+		},
+		nil)
+	if err != nil {
+		return nil, err
+	}
+	return &resourceGroupResp.ResourceGroup, nil
+}
+
+func cleanup(ctx context.Context, cred azcore.TokenCredential) (*http.Response, error) {
+	resourceGroupClient := armresources.NewResourceGroupsClient(subscriptionID, cred, nil)
+
+	pollerResp, err := resourceGroupClient.BeginDelete(ctx, resourceGroupName, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := pollerResp.PollUntilDone(ctx, 10*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	return resp.RawResponse, nil
+}
---
+++ sql/virtual_network_rule/main.go
@@ -0,0 +1,237 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/sql/armsql"
+)
+
+var (
+	subscriptionID         string
+	location               = "westus"
+	resourceGroupName      = "sample-resource-group"
+	serverName             = "sample2server"
+	virtualNetworkName     = "sample-virtual-network"
+	subnetName             = "sample-subnet"
+	virtualNetworkRuleName = "sample-virtual-network-rule"
+)
+
+func main() {
+	subscriptionID = os.Getenv("AZURE_SUBSCRIPTION_ID")
+	if len(subscriptionID) == 0 {
+		log.Fatal("AZURE_SUBSCRIPTION_ID is not set.")
+	}
+
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+
+	resourceGroup, err := createResourceGroup(ctx, cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("resources group:", *resourceGroup.ID)
+
+	server, err := createServer(ctx, cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("server:", *server.ID)
+
+	virtualNetwork, err := createVirtualNetwork(ctx, cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("virtual network:", *virtualNetwork.ID)
+
+	subnet, err := createSubnet(ctx, cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("subnet:", *subnet.ID)
+
+	virtualNetworkRule, err := createVirtualNetworkRule(ctx, cred, *subnet.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("virtual network rule:", *virtualNetworkRule.ID)
+
+	virtualNetworkRule, err = getVirtualNetworkRule(ctx, cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("get virtual network rule:", *virtualNetworkRule.ID)
+
+	keepResource := os.Getenv("KEEP_RESOURCE")
+	if len(keepResource) == 0 {
+		_, err := cleanup(ctx, cred)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("cleaned up successfully.")
+	}
+}
+
+func createServer(ctx context.Context, cred azcore.TokenCredential) (*armsql.Server, error) {
+	serversClient := armsql.NewServersClient(subscriptionID, cred, nil)
+
+	pollerResp, err := serversClient.BeginCreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		serverName,
+		armsql.Server{
+			TrackedResource: armsql.TrackedResource{
+				Location: to.StringPtr(location),
+			},
+			Properties: &armsql.ServerProperties{
+				AdministratorLogin:         to.StringPtr("dummylogin"),
+				AdministratorLoginPassword: to.StringPtr("QWE123!@#"),
+			},
+		},
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := pollerResp.PollUntilDone(ctx, 10*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	return &resp.Server, nil
+}
+
+func createVirtualNetwork(ctx context.Context, cred azcore.TokenCredential) (*armnetwork.VirtualNetwork, error) {
+	virtualNetworkClient := armnetwork.NewVirtualNetworksClient(subscriptionID, cred, nil)
+
+	pollerResp, err := virtualNetworkClient.BeginCreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		virtualNetworkName,
+		armnetwork.VirtualNetwork{
+			Resource: armnetwork.Resource{
+				Location: to.StringPtr(location),
+			},
+			Properties: &armnetwork.VirtualNetworkPropertiesFormat{
+				AddressSpace: &armnetwork.AddressSpace{
+					AddressPrefixes: []*string{
+						to.StringPtr("10.1.0.0/16"),
+					},
+				},
+			},
+		},
+		nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := pollerResp.PollUntilDone(ctx, 10*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	return &resp.VirtualNetwork, nil
+}
+
+func createSubnet(ctx context.Context, cred azcore.TokenCredential) (*armnetwork.Subnet, error) {
+	subnetsClient := armnetwork.NewSubnetsClient(subscriptionID, cred, nil)
+
+	pollerResp, err := subnetsClient.BeginCreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		virtualNetworkName,
+		subnetName,
+		armnetwork.Subnet{
+			Properties: &armnetwork.SubnetPropertiesFormat{
+				AddressPrefix: to.StringPtr("10.1.0.0/24"),
+			},
+		},
+		nil,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+	resp, err := pollerResp.PollUntilDone(ctx, 10*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	return &resp.Subnet, nil
+}
+
+func createVirtualNetworkRule(ctx context.Context, cred azcore.TokenCredential, subnetID string) (*armsql.VirtualNetworkRule, error) {
+	virtualNetworkRulesClient := armsql.NewVirtualNetworkRulesClient(subscriptionID, cred, nil)
+
+	pollerResp, err := virtualNetworkRulesClient.BeginCreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		serverName,
+		virtualNetworkRuleName,
+		armsql.VirtualNetworkRule{
+			Properties: &armsql.VirtualNetworkRuleProperties{
+				VirtualNetworkSubnetID:           to.StringPtr(subnetID),
+				IgnoreMissingVnetServiceEndpoint: to.BoolPtr(true),
+			},
+		},
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := pollerResp.PollUntilDone(ctx, 10*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	return &resp.VirtualNetworkRule, nil
+}
+
+func getVirtualNetworkRule(ctx context.Context, cred azcore.TokenCredential) (*armsql.VirtualNetworkRule, error) {
+	virtualNetworkRulesClient := armsql.NewVirtualNetworkRulesClient(subscriptionID, cred, nil)
+
+	resp, err := virtualNetworkRulesClient.Get(ctx, resourceGroupName, serverName, virtualNetworkRuleName, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &resp.VirtualNetworkRule, nil
+}
+
+func createResourceGroup(ctx context.Context, cred azcore.TokenCredential) (*armresources.ResourceGroup, error) {
+	resourceGroupClient := armresources.NewResourceGroupsClient(subscriptionID, cred, nil)
+
+	resourceGroupResp, err := resourceGroupClient.CreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		armresources.ResourceGroup{
+			Location: to.StringPtr(location),
+		},
+		nil)
+	if err != nil {
+		return nil, err
+	}
+	return &resourceGroupResp.ResourceGroup, nil
+}
+
+func cleanup(ctx context.Context, cred azcore.TokenCredential) (*http.Response, error) {
+	resourceGroupClient := armresources.NewResourceGroupsClient(subscriptionID, cred, nil)
+
+	pollerResp, err := resourceGroupClient.BeginDelete(ctx, resourceGroupName, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := pollerResp.PollUntilDone(ctx, 10*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	return resp.RawResponse, nil
+}

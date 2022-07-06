2432c93dc17fc6714254e39bb6f67c0cd025b785
Jiahui Peng <46921893+Alancere@users.noreply.github.com>
2022-07-06 17:44:55 +0800

    add recoveryservice armrecoveryservice sample (#351)
    * add recoveryservice armrecoveryservice sample
    
    * add LICENSE

---
+++ sdk/resourcemanager/recoveryservices/vaultextendedinfo/main.go
@@ -0,0 +1,177 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/recoveryservices/armrecoveryservices"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
+)
+
+var (
+	subscriptionID    string
+	location          = "westus"
+	resourceGroupName = "sample-resource-group"
+	vaultName         = "sample-recoveryservice-vault"
+)
+
+func main() {
+	subscriptionID = os.Getenv("AZURE_SUBSCRIPTION_ID")
+	if len(subscriptionID) == 0 {
+		log.Fatal("AZURE_SUBSCRIPTION_ID is not set.")
+	}
+
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+
+	resourceGroup, err := createResourceGroup(ctx, cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("resources group:", *resourceGroup.ID)
+
+	vault, err := createRecoveryServiceVault(ctx, cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("recovery service vault:", *vault.ID)
+
+	vaultExtendedInfo, err := createVaultExtendedInfo(ctx, cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("recovery service vault extended info:", *vaultExtendedInfo.ID)
+
+	vaultExtendedInfo, err = getVaultExtendedInfo(ctx, cred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("get recovery service vault extended info:", *vaultExtendedInfo.ID)
+
+	keepResource := os.Getenv("KEEP_RESOURCE")
+	if len(keepResource) == 0 {
+		err = cleanup(ctx, cred)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("cleaned up successfully.")
+	}
+}
+
+func createRecoveryServiceVault(ctx context.Context, cred azcore.TokenCredential) (*armrecoveryservices.Vault, error) {
+	vaultClient, err := armrecoveryservices.NewVaultsClient(subscriptionID, cred, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	pollerResp, err := vaultClient.BeginCreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		vaultName,
+		armrecoveryservices.Vault{
+			Location: to.Ptr(location),
+			SKU: &armrecoveryservices.SKU{
+				Name: to.Ptr(armrecoveryservices.SKUNameStandard),
+			},
+			Properties: &armrecoveryservices.VaultProperties{},
+		},
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := pollerResp.PollUntilDone(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &resp.Vault, err
+}
+
+func createVaultExtendedInfo(ctx context.Context, cred azcore.TokenCredential) (*armrecoveryservices.VaultExtendedInfoResource, error) {
+	vaultExtendedInfoClient, err := armrecoveryservices.NewVaultExtendedInfoClient(subscriptionID, cred, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := vaultExtendedInfoClient.CreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		vaultName,
+		armrecoveryservices.VaultExtendedInfoResource{
+			Properties: &armrecoveryservices.VaultExtendedInfo{
+				Algorithm: to.Ptr("None"),
+				//IntegrityKey: to.StringPtr(),
+			},
+		},
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &resp.VaultExtendedInfoResource, err
+}
+
+func getVaultExtendedInfo(ctx context.Context, cred azcore.TokenCredential) (*armrecoveryservices.VaultExtendedInfoResource, error) {
+	vaultExtendedInfoClient, err := armrecoveryservices.NewVaultExtendedInfoClient(subscriptionID, cred, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := vaultExtendedInfoClient.Get(ctx, resourceGroupName, vaultName, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &resp.VaultExtendedInfoResource, err
+}
+
+func createResourceGroup(ctx context.Context, cred azcore.TokenCredential) (*armresources.ResourceGroup, error) {
+	resourceGroupClient, err := armresources.NewResourceGroupsClient(subscriptionID, cred, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resourceGroupResp, err := resourceGroupClient.CreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		armresources.ResourceGroup{
+			Location: to.Ptr(location),
+		},
+		nil)
+	if err != nil {
+		return nil, err
+	}
+	return &resourceGroupResp.ResourceGroup, nil
+}
+
+func cleanup(ctx context.Context, cred azcore.TokenCredential) error {
+	resourceGroupClient, err := armresources.NewResourceGroupsClient(subscriptionID, cred, nil)
+	if err != nil {
+		return err
+	}
+
+	pollerResp, err := resourceGroupClient.BeginDelete(ctx, resourceGroupName, nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = pollerResp.PollUntilDone(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
